6aa435a4ca0cdad248cc079f064a4ec2f6c446e9
AlexTClarke <119004428+AlexTClarke@users.noreply.github.com>
2024-04-14 10:23:39 +0100

    openai now

--- tts.py
+++ tts.py
@@ -1,9 +1,9 @@
 # from gtts import gTTS
 from pygame import mixer
 import os
-import pyttsx3
+from openai import OpenAI
 mixer.init()
-engine = pyttsx3.init()
+client = OpenAI( api_key="")
 def text_to_speech(text, language='en', filename='output2.mp3'):
     """
     Convert text to speech and save it as an MP3 file.
@@ -14,24 +14,27 @@ def text_to_speech(text, language='en', filename='output2.mp3'):
         filename (str, optional): The filename for the output MP3 file (default is 'output.mp3').
     """
     # tts = gTTS(text=text, lang=language, slow=False)
-    engine.say(text)
-    engine.runAndWait()
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=text
+        )
 
-    # tts.save(filename)
+    response.stream_to_file(filename)
     # print(f"Saved TTS as '{filename}'")
 
 
-# def play_audio(filename='output2.mp3'):
-#     """
-#     Play the audio file.
+def play_audio(filename='output2.mp3'):
+    """
+    Play the audio file.
     
-#     Args:
-#         filename (str, optional): The filename of the audio file to play (default is 'output.mp3').
-#     """
-#     mixer.music.load(filename)
-#     mixer.music.play()
-#     while mixer.music.get_busy():
-#         pass
+    Args:
+        filename (str, optional): The filename of the audio file to play (default is 'output.mp3').
+    """
+    mixer.music.load(filename)
+    mixer.music.play()
+    while mixer.music.get_busy():
+        pass
 
 
 def play_score(player1,player2, handout = False):
@@ -39,7 +42,7 @@ def play_score(player1,player2, handout = False):
     if handout:
         text += " Handout"
     text_to_speech(text)
-    # play_audio()
+    play_audio()
 
 if __name__ == "__main__":
     play_score(5, 3, True)
