9ac8e9d1d3651fbdc8a2de8c9b8f481fe8e1a7ae
Kevin Roman <44275689+Kevin-Roman@users.noreply.github.com>
2024-04-14 11:42:59 +0100

    Websocket support

--- preprocessing.py
+++ preprocessing.py
@@ -2,9 +2,11 @@
 
 import cv2
 import numpy as np
+
 # take in 4 videos
 # iterate through the videos and take a frame from each video
 # merge the 4 frames into one big image
+
 
 def take_frame(video_path1, video_path2, video_path3, video_path4):
     # take frames until the end of the video
@@ -12,7 +14,7 @@ def take_frame(video_path1, video_path2, video_path3, video_path4):
     cap2 = cv2.VideoCapture(video_path2)
     cap3 = cv2.VideoCapture(video_path3)
     cap4 = cv2.VideoCapture(video_path4)
-    
+
     for x in range(500):
 
         # take the next frame from each video
@@ -32,10 +34,10 @@ def take_frame(video_path1, video_path2, video_path3, video_path4):
     cap3.release()
     cap4.release()
 
-
     return merge(frame1, frame2, frame3, frame4)
 
-def merge(img1,img2,img3,img4):
+
+def merge(img1, img2, img3, img4):
 
     # merge 4 images into one big long vertical strip with a 1px gap between each variable size image
 
@@ -44,7 +46,7 @@ def merge(img1,img2,img3,img4):
     img2 = cv2.rotate(img2, cv2.ROTATE_90_CLOCKWISE)
     h2, w2 = img2.shape[:2]
     # img3 = cv2.rotate(img2, cv2.ROTATE_90_CLOCKWISE)
-    #rotate img3 90 degrees
+    # rotate img3 90 degrees
     h3, w3 = img3.shape[:2]
     h4, w4 = img4.shape[:2]
 
@@ -52,36 +54,36 @@ def merge(img1,img2,img3,img4):
     max_width = max(w1, w2, w3, w4)
 
     # create a blank image with the max width and the sum of the heights
-    blank_image = np.zeros((h1+h2+h3+h4+3, max_width, 3), np.uint8)
+    blank_image = np.zeros((h1 + h2 + h3 + h4 + 3, max_width, 3), np.uint8)
 
     # fill the blank image with the 4 images
 
     blank_image[0:h1, 0:w1] = img1
-    blank_image[h1+1:h1+h2+1, 0:w2] = img2
-    blank_image[h1+h2+2:h1+h2+h3+2, 0:w3] = img3
+    blank_image[h1 + 1 : h1 + h2 + 1, 0:w2] = img2
+    blank_image[h1 + h2 + 2 : h1 + h2 + h3 + 2, 0:w3] = img3
 
-    blank_image[h1+h2+h3+3:h1+h2+h3+h4+3, 0:w4] = img4
+    blank_image[h1 + h2 + h3 + 3 : h1 + h2 + h3 + h4 + 3, 0:w4] = img4
 
     # save the big image
-    cv2.imwrite('big_image.png', blank_image)
+    cv2.imwrite("big_image.png", blank_image)
     return blank_image
+
 
 def split(big_image):
 
     # split the big image into 4 images
-    big_image = cv2.imread('big_image.png')
-    
+    big_image = cv2.imread("big_image.png")
+
     # get the dimensions of the big image
     h, w = big_image.shape[:2]
 
     # split the big image into the 4 original images with original widths
-    
-    img1 = big_image[0:h//4, 0:w]
-    img2 = big_image[h//4+1:h//2+1, 0:w]
-    img3 = big_image[h//2+2:3*h//4+2, 0:w]
 
-    img4 = big_image[3*h//4+3:h, 0:w]
+    img1 = big_image[0 : h // 4, 0:w]
+    img2 = big_image[h // 4 + 1 : h // 2 + 1, 0:w]
+    img3 = big_image[h // 2 + 2 : 3 * h // 4 + 2, 0:w]
 
+    img4 = big_image[3 * h // 4 + 3 : h, 0:w]
 
     # save the 4 images
     # cv2.imwrite('new_frame1.png', img1)
@@ -89,9 +91,10 @@ def split(big_image):
     # cv2.imwrite('new_frame3.png', img3)
     # cv2.imwrite('new_frame4.png', img4)
 
-frames = take_frame('backmidwall.mp4','backwall.mp4','floor.mp4','topview.mp4')
-for frame1,frame2,frame3,frame4 in frames:
-    frames = [frame1,frame2,frame3,frame4]
 
-big_image = merge( *frames)
+frames = take_frame("backmidwall.mp4", "backwall.mp4", "floor.mp4", "topview.mp4")
+for frame1, frame2, frame3, frame4 in frames:
+    frames = [frame1, frame2, frame3, frame4]
+
+big_image = merge(*frames)
 split(big_image)
--- tts.py
+++ tts.py
@@ -1,33 +1,34 @@
 # from gtts import gTTS
-from pygame import mixer
-import os
+import asyncio
+
+import websockets
 from openai import OpenAI
+from pygame import mixer
+
 mixer.init()
-client = OpenAI( api_key="")
-def text_to_speech(text, language='en', filename='output2.mp3'):
+client = OpenAI(api_key="")
+
+
+def text_to_speech(text, language="en", filename="output2.mp3"):
     """
     Convert text to speech and save it as an MP3 file.
-    
+
     Args:
         text (str): The text to be converted to speech.
         language (str, optional): The language code (default is 'en' for English).
         filename (str, optional): The filename for the output MP3 file (default is 'output.mp3').
     """
     # tts = gTTS(text=text, lang=language, slow=False)
-    response = client.audio.speech.create(
-        model="tts-1",
-        voice="alloy",
-        input=text
-        )
+    response = client.audio.speech.create(model="tts-1", voice="alloy", input=text)
 
     response.stream_to_file(filename)
     # print(f"Saved TTS as '{filename}'")
 
 
-def play_audio(filename='output2.mp3'):
+def play_audio(filename="output2.mp3"):
     """
     Play the audio file.
-    
+
     Args:
         filename (str, optional): The filename of the audio file to play (default is 'output.mp3').
     """
@@ -37,12 +38,23 @@ def play_audio(filename='output2.mp3'):
         pass
 
 
-def play_score(player1,player2, handout = False):
+def play_score(player1, player2, handout=False):
     text = f"{player1} {player2}"
     if handout:
         text += " Handout"
     text_to_speech(text)
     play_audio()
 
-if __name__ == "__main__":
-    play_score(5, 3, True)
+
+async def receive_data():
+    uri = "ws://localhost:8080/ws"
+    async with websockets.connect(uri) as websocket:
+        try:
+            while True:
+                data = await websocket.recv()
+                print("Received:", data)
+        except websockets.exceptions.ConnectionClosed:
+            print("WebSocket connection closed")
+
+
+asyncio.run(receive_data())
